5109352604b1c182c0f6816536dbef4bc50bb024
Kyriakos Sergiou <serghiouk@gmail.com>
2022-05-18 14:28:02 +0100

    new

---
+++ news_df.py
@@ -0,0 +1,37 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+finviz_url = 'https://finviz.com/quote.ashx?t='
+
+news_tables = {}
+
+for ticker in tickers:
+    url = finviz_url + ticker
+
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
+
+    html = BeautifulSoup(response, features='html.parser')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
+
+    for row in news_table.findAll('tr'):
+
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([ticker, date, time, title])
+
+df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+
+df.to_csv('news_df.csv',index=False)
