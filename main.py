da8446bd63185eb9db727a680682ddb983b75f09
Kyriakos Sergiou <serghiouk@gmail.com>
2022-05-18 13:28:21 +0100

    new

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+finviz_url = 'https://finviz.com/quote.ashx?t='
+tickers = ['AMZN', 'GOOG', 'FB']
 
+news_tables = {}
+for ticker in tickers:
+    url = finviz_url + ticker
+
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
+
+    html = BeautifulSoup(response, features='html.parser')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
+
+    for row in news_table.findAll('tr'):
+
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([ticker, date, time, title])
+
+df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+
+vader = SentimentIntensityAnalyzer()
+
+f = lambda title: vader.polarity_scores(title)['compound']
+df['compound'] = df['title'].apply(f)
+df['date'] = pd.to_datetime(df.date).dt.date
+
+plt.figure(figsize=(10,8))
+mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+mean_df = mean_df.xs('compound', axis="columns")
+mean_df.plot(kind='bar')
+plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#rtgkkkl
+   None
